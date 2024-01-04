7668f500ab4e69e85382414c8a74ff2aac96a998
Mireia <mireia@beateam.es>
2024-01-04 13:53:41 +0100

    Añadir rutas a páginas de login, lista y detalles

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { LoginPage } from "./login";
+import { ListPage } from "./list";
+import { DetailPage } from "./detail";
 
 export const App = () => {
-  return <h1>Hello React !!</h1>;
+  return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/list" element={<ListPage/>} />
+            <Route path="/detail" element={<DetailPage/>} />
+        </Routes>
+    </Router>
+  );
 };
---
+++ src/login.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export const LoginPage: React.FC = () => {
+    const navigate = useNavigate();
+    const handleNavigation = () => {
+        navigate("/list");
+    }
+  return (
+    <>
+      <h2>Hello from login page</h2>
+      <button onClick={handleNavigation}>List</button>
+    </>
+  );
+};
