9ea069967500578c1703d1ccda08a541d42eed63
Mireia <mireia@beateam.es>
2024-01-04 14:06:27 +0100

    Implementar login

--- src/login.tsx
+++ src/login.tsx
@@ -3,13 +3,31 @@ import { Link, useNavigate } from "react-router-dom";
 
 export const LoginPage: React.FC = () => {
     const navigate = useNavigate();
-    const handleNavigation = () => {
-        navigate("/list");
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const handleNavigation = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(username === "admin" && password === "test") {
+            navigate("/list");
+        } else {
+            alert("Invalid credentials");
+        }
     }
   return (
-    <>
-      <h2>Hello from login page</h2>
-      <button onClick={handleNavigation}>List</button>
-    </>
+    <form onSubmit={handleNavigation}>
+        <h2>Hello from login page</h2>
+      <div className="login-container">
+        <div>
+          <label>Username:</label>
+          <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)}/>
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+        </div>
+      <button type="submit">Go</button>
+      </div>
+    </form>
   );
 };
